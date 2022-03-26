40680a2e7ab98f2fcded0ff7ecda9362c0157ec3
Spencer10798 <maghrouri@hotmail.com>
2022-03-26 16:05:44 -0400

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       Main App
     </div>
   );
---
+++ src/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      Header
+    </div>
+  );
+}
+
+export default NavBar;
